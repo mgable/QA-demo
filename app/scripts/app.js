44a22ae4f1ded080cf1905ec128c5b78650f61a3
Mark Gable <mgable43@gmail.com>
2015-03-12 14:15:02 -0700

    angular framework

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name marketshareQaDemoApp
+ * @description
+ * # marketshareQaDemoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('marketshareQaDemoApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name marketshareQaDemoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the marketshareQaDemoApp
+ */
+angular.module('marketshareQaDemoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
