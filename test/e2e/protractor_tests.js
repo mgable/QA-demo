271963d8da083ffb3d3f19c4f7a454b0924eeb50
Mark Gable <mgable43@gmail.com>
2015-03-12 17:03:26 -0700

    finish up protractor tests

--- test/e2e/protractor_tests.js
+++ test/e2e/protractor_tests.js
@@ -10,37 +10,67 @@ describe('Scenario Templates', function() {
 
 	describe("name field", function(){
 
-		it("should require a template name", function(){
-
+		it("should require a name", function(){
+			expect(element(by.css('#submitButton')).getAttribute('disabled')).toBeTruthy();
 		});	
 
-		it("should require a template name of at least 2 characters", function(){
-
+		it("should require a name of at least 2 characters", function(){
+			var inputField = element(by.model("data.name"));
+			inputField.sendKeys("x");
+			expect(element(by.css('#submitButton')).getAttribute('disabled')).toBeTruthy();
+			inputField.sendKeys("x");
+			expect(element(by.css('#submitButton')).getAttribute('disabled')).toBeFalsy();
 		});	
 
-		it("should prohibit a template name over 256 characters", function(){
-
+		it("should prohibit a name over 256 characters", function(){
+			var inputField = element(by.model("data.name"));
+			inputField.sendKeys("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+			expect(element(by.css('#submitButton')).getAttribute('disabled')).toBeFalsy();
+			inputField.sendKeys("x");
+			expect(element(by.css('#submitButton')).getAttribute('disabled')).toBeTruthy();
 		});	
 
-		it("should prohibit templates names with \ / ? : * \" > < | characters", function(){
-
+		it("should prohibit names with \ / ? : * \" > < | characters", function(){
+			var inputField = element(by.model("data.name"));
+			inputField.clear();
+			inputField.sendKeys("xx/");
+			expect(element(by.css('#submitButton')).getAttribute('disabled')).toBeTruthy();
+			inputField.clear();
+			inputField.sendKeys("xx\\");
+			expect(element(by.css('#submitButton')).getAttribute('disabled')).toBeTruthy();
+			inputField.clear();
+			inputField.sendKeys("xx?");
+			expect(element(by.css('#submitButton')).getAttribute('disabled')).toBeTruthy();
+			inputField.clear();
+			inputField.sendKeys("xx?");
+			expect(element(by.css('#submitButton')).getAttribute('disabled')).toBeTruthy();
+			inputField.clear();
+			inputField.sendKeys("xx:");
+			expect(element(by.css('#submitButton')).getAttribute('disabled')).toBeTruthy();
+			inputField.clear();
+			inputField.sendKeys("xx*");
+			expect(element(by.css('#submitButton')).getAttribute('disabled')).toBeTruthy();
+			inputField.clear();
+			inputField.sendKeys("xx\"");
+			expect(element(by.css('#submitButton')).getAttribute('disabled')).toBeTruthy();
+			inputField.clear();
+			inputField.sendKeys("xx>");
+			expect(element(by.css('#submitButton')).getAttribute('disabled')).toBeTruthy();
+			inputField.clear();
+			inputField.sendKeys("xx<");
+			expect(element(by.css('#submitButton')).getAttribute('disabled')).toBeTruthy();
+			inputField.clear();
+			inputField.sendKeys("xx|");
+			expect(element(by.css('#submitButton')).getAttribute('disabled')).toBeTruthy();
 		});	
 
-		it("should require a template name to be unique", function(){
-
+		it("should require a name to be unique", function(){
+			var inputField = element(by.model("data.name"));
+			inputField.clear();
+			inputField.sendKeys("one");
+			expect(element(by.css('#submitButton')).getAttribute('disabled')).toBeTruthy();
+			expect(element(by.css('#alert')).isPresent()).toBeTruthy();
 		});
-	});
-
-	describe("description field", function(){
-
-		it("should not be required", function(){
-
-		});		
-
-		it("should prohibit a description over 256 characters", function(){
-
-		});	
-
 	});
 
 });
